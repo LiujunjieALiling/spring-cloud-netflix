b39658a357b02756bce1a30ed1f253a1028d7cab
LiujunjieALiling <smartliujunjie@163.com>
2020-08-31 19:15:02 +0800

    修改测试类

--- hystrix-app/src/main/java/com/eureka/hystrix/CommandTest.java
+++ hystrix-app/src/main/java/com/eureka/hystrix/CommandTest.java
@@ -3,6 +3,7 @@ package com.eureka.hystrix;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import rx.Observable;
+import rx.Subscription;
 
 import java.util.concurrent.Future;
 
@@ -18,15 +19,20 @@ import static junit.framework.Assert.assertEquals;
  */
 public class CommandTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  Exception{
 
+        System.out.println("==============================Synchronous调用=============================");
         // Synchronous,  等价于 queue().get()
         assertEquals("Hello World!", new CommandHelloWorld("World").execute());
         assertEquals("Hello Bob!", new CommandHelloWorld("Bob").execute());
 
+
+        System.out.println("==============================Asynchronous调用=============================");
         // asynchronous，调用 #toObservable() 方法的基础上，调用：Observable#toBlocking() 和 BlockingObservable#toFuture() 返回 Future
         Future<String> f = new CommandHelloWorld("Bob").queue();
+        System.out.println(String.format("main方法中运行获取执行结果得线程名：%s，返回值：%s" ,Thread.currentThread().getName(),f.get()));
 
+        System.out.println("==============================Observable调用===============================");
         // asynchronous，调用 #toObservable() 方法，并向 Observable 注册 rx.subjects.ReplaySubject 发起订阅。
         Observable<String> o = new CommandHelloWorld("Bob").observe();
     }
@@ -42,6 +48,8 @@ class CommandHelloWorld extends HystrixCommand<String>{ //  // HystrixObservable
 
     @Override
     protected String run() throws Exception {
+
+        System.out.println(String.format("CommandHelloWorld中运行run方法得线程名：%s" ,Thread.currentThread().getName()));
 
         return "Hello " + name + "!";
     }
