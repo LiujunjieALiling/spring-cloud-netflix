94b6d6f9acd3ce3823d8dc68e88f9da8a98441a6
LiujunjieALiling <smartliujunjie@163.com>
2020-09-07 09:47:18 +0800

    修改注释信息

--- hystrix-dashboard/src/main/java/com/eureka/hystrix/dashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/eureka/hystrix/dashboard/HystrixDashboardApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
- * 1. 访问http://localhost:8084/hystrix 可以查看Dashboard
- * 2. 在输入框中填入: (1) http://localhost:8083/actuator/hystrix.stream
+ * 1. 访问http://localhost:8085/hystrix 可以查看Dashboard
+ * 2. 在输入框中填入: (1) http://localhost:8083/actuator/hystrix.stream （单体应用）
+ *                     http://localhost:8086/turbine.stream?cluster=HYSTRIX-APP （ HYSTRIX-APP集群 ）
  *                 (2) 输入Delay毫秒数
  *                 (3) 输入Title名称
  * 注意：首先要先调用一下想监控的服务的API，否则将会显示一个空的图表.
